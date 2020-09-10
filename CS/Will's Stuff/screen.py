931115085e43304df85280c287e955c6909eda6d
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-09-10 04:42:11 -0400

    Adjusted Python to use the MIPI CSI camera

--- CS/Will's Stuff/screen.py
+++ CS/Will's Stuff/screen.py
@@ -24,10 +24,46 @@ b4.grid(row=1, column=1)
 
 
 
+# Based off of https://github.com/JetsonHacksNano/CSI-Camera/blob/master/simple_camera.py
+# This returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values of 0 and 2)
+# display_width and display_height determine the size of the window on the screen
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+
 #   Capture video frames
 lmain = tk.Label(imageFrame)
 lmain.grid(row=0, column=0)
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
 def show_frame():
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -37,6 +73,12 @@ def show_frame():
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(10, show_frame) 
+
+
+
+
+
+
 
 
 
