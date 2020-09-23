24fb83c3721b4e8b54d53af3bd8ea4e474d0742f
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-09-22 22:04:17 -0400

    Added more info to dependencies; updated sending video

--- CS/Josh's Stuff/Prototypes/20-09-16 Socket and Canny/Socket Demo - Live Video/sends_video.py
+++ CS/Josh's Stuff/Prototypes/20-09-16 Socket and Canny/Socket Demo - Live Video/sends_video.py
@@ -33,8 +33,50 @@ s = Video_Sender()
 print("Video Sender initialized. Waiting for connection to send video.")
 s.WaitForConnection()
 
+
+# Based off of https://github.com/JetsonHacksNano/CSI-Camera/blob/master/simple_camera.py
+# This returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values of 0 and 2)
+# display_width and display_height determine the size of the window on the screen
+
+def gstreamer_pipeline(
+#    capture_width=1280,
+#    capture_height=720,
+#    display_width=1280,
+#    display_height=720,
+#    framerate=60,
+#    flip_method=0,
+    capture_width=1280,
+    capture_height=720,
+    display_width=320,
+    display_height=180,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
 # Initialize camera
-camera = cv2.VideoCapture(0)  
+camera = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
+# camera = cv2.VideoCapture(4)  
 
 # Loop to send the video, frame by frame.
 while True: 
