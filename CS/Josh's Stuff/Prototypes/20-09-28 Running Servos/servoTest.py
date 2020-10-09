a7b95dd67cd3c0dbd21ca269d3284a7749f68180
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-10-09 15:12:51 -0400

    Created working test of the servos!

---
+++ CS/Josh's Stuff/Prototypes/20-09-28 Running Servos/servoTest.py
@@ -0,0 +1,15 @@
+from adafruit_servokit import ServoKit 
+import time # For pausing
+
+myKit = ServoKit(channels=16)
+myKit.servo[0].angle = 180
+
+pause_time = .01
+
+for i in range(0, 180, 1):
+    myKit.servo[0].angle = i
+    time.sleep(pause_time)
+
+for i in range(180, 0, -1):
+    myKit.servo[0].angle = i
+    time.sleep(pause_time)
