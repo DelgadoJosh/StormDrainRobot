f6d345df2b2d9fcd8e457f6ec83a2adaa855a0e8
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2020-10-12 15:05:32 -0400

    Created an updated GUI

--- CS/Josh's Stuff/Prototypes/20-10-12 GUI testing/screen.py
+++ CS/Josh's Stuff/Prototypes/20-10-12 GUI testing/screen.py
@@ -9,6 +9,7 @@ from datetime import datetime
 window = tk.Tk()  #Makes main window
 window.geometry("1800x1000")
 window.wm_title("Senior Design")
+window.configure(bg="gray17")
 #window.config(background="#FFFFFF")
 
 # Must be defined above Graphics window
@@ -24,7 +25,7 @@ def screenshot():
 
 def testFunction():
     distance = "1000.5"
-    GPS = "1234 Latitude, 4567 Longitude"
+    GPS = "\n1234 Latitude, \n4567 Longitude"
     battery = "79"
     orientation = "North"
     return f"Distance: {distance} \n GPS: {GPS} \n Battery: {battery} \n Orientation: {orientation}"
@@ -33,19 +34,20 @@ def testFunction():
 imageFrame = tk.Frame(window, width=1280, height=720)
 imageFrame.grid(row=1, column=0, columnspan = 5, rowspan = 5, padx=10, pady=10)
 
-tk_object_height = '5'
+tk_object_height = '2' #'5'
 tk_object_width = '30'
 
-b1 = tk.Button(window, text="Stuff", bg='gray', height=tk_object_height, width=tk_object_width, font=("Helvetica", 16))
-b2 = tk.Button(window, text="Quit", bg='gray', height=tk_object_height, width=tk_object_width, command=quit, font=("Helvetica", 16))
-b3 = tk.Button(window, text="Screenshot", bg='gray', height=tk_object_height, width=tk_object_width, command=screenshot, font=("Helvetica", 16))
-b4 = tk.Button(window, text="placeholder", bg='gray', height=tk_object_height, width=tk_object_width, font=("Helvetica", 16))
-data = tk.Label(window, bg='gray', height=tk_object_height, width=tk_object_width, text=testFunction(), font=("Helvetica", 16))
+button_bg_color='gray30' # "darkgray"
+b1 = tk.Button(window, text="Export .Shape File", bg=button_bg_color, height=tk_object_height, width=tk_object_width, font=("Helvetica", 16))
+b2 = tk.Button(window, text="Quit", bg=button_bg_color, height=tk_object_height, width=tk_object_width, command=quit, font=("Helvetica", 16))
+b3 = tk.Button(window, text="Screenshot", bg=button_bg_color, height=tk_object_height, width=tk_object_width, command=screenshot, font=("Helvetica", 16))
+# b4 = tk.Button(window, text="", bg='gray', height=tk_object_height, width=tk_object_width, font=("Helvetica", 16))
+data = tk.Label(window, bg='gray30', height=21, width=tk_object_width, text=testFunction(), font=("Helvetica", 16)) #anchor="w"
 data.grid(row=1, column=6)
 b1.grid(row=2, column=6)
-b4.grid(row=3, column=6)
-b3.grid(row=4, column=6)
-b2.grid(row=5, column=6)
+# b4.grid(row=2, column=6)
+b3.grid(row=3, column=6)
+b2.grid(row=4, column=6)
 
 # Based off of https://github.com/JetsonHacksNano/CSI-Camera/blob/master/simple_camera.py
 # This returns a GStreamer pipeline for capturing from the CSI camera
@@ -121,8 +123,8 @@ def show_frame():
 
 
 #Slider window (slider controls stage position)
-sliderFrame = tk.Frame(window, width=600, height=100)
-sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+# sliderFrame = tk.Frame(window, width=600, height=100)
+# sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
 
 
 show_frame()  #Display 2
