9c7aa49cbe618dbc79165841ca007c6e91ce1513
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-10-19 12:15:20 -0400

    Added Test Scripts

---
+++ CS/Josh's Stuff/Prototypes/20-10-19 Startup/test.py
@@ -0,0 +1,41 @@
+"""
+Basically testing what happens when you run a script upon startup
+
+python3 test.py
+
+python3 "Desktop/TeamBlack/Github\ Repo/StormDrainRobot/CS/Josh\'s Stuff/Prototypes/20-10-19\ Startup/test.py"
+
+/Desktop/TeamBlack/Github Repo/StormDrainRobot/CS/Josh's Stuff
+
+Desktop/TeamBlack/Github\ Repo/StormDrainRobot/CS/Josh\'s\ Stuff/Prototypes/20-10-19\ Startup/
+
+python3 Desktop/TeamBlack/Github\ Repo/StormDrainRobot/CS/Josh\'s\ Stuff/Prototypes/20-10-19\ Startup/test.py
+
+
+Adjust test_script.sh
+
+Then run command to make it runnable
+chmod u+x test_script.sh
+
+Then run it with:
+./test_script.sh
+
+Or
+./Desktop/TeamBlack/Github\ Repo/StormDrainRobot/CS/Josh\'s\ Stuff/Prototypes/20-10-19\ Startup/test_script.sh
+
+
+Use to add startup tasks
+crontab -e
+
+
+@reboot /path/to/script
+
+Change editor:
+select-editor
+
+
+
+
+"""
+
+print("Hello World")
---
+++ CS/Josh's Stuff/Prototypes/20-10-19 Startup/test_script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 Desktop/TeamBlack/Github\ Repo/StormDrainRobot/CS/Josh\'s\ Stuff/Prototypes/20-10-19\ Startup/test.py
