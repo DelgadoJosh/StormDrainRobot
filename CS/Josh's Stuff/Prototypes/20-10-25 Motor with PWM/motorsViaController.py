dc3aa147c2867df4fa5d4e3854fa1b5e9a87b43e
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2020-10-27 14:19:15 -0400

    Additional notes

--- CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
+++ CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
@@ -25,3 +25,24 @@ time.sleep(1)
 # for i in range(100, 0, -1):
 #     myKit.continuous_servo[1].throttle = i
 #     time.sleep(pause_time)
+
+
+
+# Maybe use servo driver to support pwm
+# https://learn.adafruit.com/16-channel-pwm-servo-driver/python-circuitpython
+# led_channel = pca.channels[0]
+# led_channel.duty_cycle = 0xffff
+
+# Control IC
+# https://circuitpython.readthedocs.io/projects/pca9685/en/latest/api.html
+
+# sudo pip install adafruit-circuitpython-pca9685 
+
+
+# from board import SCL, SDA
+# import busio 
+# from adafruit_pca9685 import PCA9685 
+# i2c_bus = busio.I2C(SCL, SDA) 
+# pca = PCA9685(i2c_bus) 
+# pca.frequency = 60
+# pca.channels[0].duty_cycle = 0x7FFF
