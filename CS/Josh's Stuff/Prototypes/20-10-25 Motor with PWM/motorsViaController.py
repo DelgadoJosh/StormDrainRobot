fd569bc5482f6b0b70aca35d8504be8bdc269855
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-10-27 17:55:42 -0400

    Initial draft of motors via the servo controller

--- CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
+++ CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
@@ -1,21 +1,21 @@
-# https://www.youtube.com/watch?v=8YKAtpPSEOk
-# https://learn.adafruit.com/16-channel-pwm-servo-driver/python-circuitpython
-from adafruit_servokit import ServoKit 
-import time # For pausing
+# # https://www.youtube.com/watch?v=8YKAtpPSEOk
+# # https://learn.adafruit.com/16-channel-pwm-servo-driver/python-circuitpython
+# from adafruit_servokit import ServoKit 
+# import time # For pausing
 
-myKit = ServoKit(channels=16)
-# myKit.servo[0].angle = 180
-myKit.continuous_servo[1].throttle = 0
+# myKit = ServoKit(channels=16)
+# # myKit.servo[0].angle = 180
+# myKit.continuous_servo[1].throttle = 0
 
-pause_time = .01
+# pause_time = .01
 
-# Full ahead
-myKit.continuous_servo[1].throttle = 1
-time.sleep(1) 
+# # Full ahead
+# myKit.continuous_servo[1].throttle = 1
+# time.sleep(1) 
 
-# Full reverse
-myKit.continuous_servo[1].throttle = -1
-time.sleep(1)
+# # Full reverse
+# myKit.continuous_servo[1].throttle = -1
+# time.sleep(1)
 
 # for i in range(0, 100, 1):
 #     # myKit.servo[0].angle = i
@@ -37,12 +37,17 @@ time.sleep(1)
 # https://circuitpython.readthedocs.io/projects/pca9685/en/latest/api.html
 
 # sudo pip install adafruit-circuitpython-pca9685 
+# python3 motorsViaController.py
 
-
-# from board import SCL, SDA
-# import busio 
-# from adafruit_pca9685 import PCA9685 
-# i2c_bus = busio.I2C(SCL, SDA) 
-# pca = PCA9685(i2c_bus) 
-# pca.frequency = 60
-# pca.channels[0].duty_cycle = 0x7FFF
+from board import SCL, SDA
+import busio 
+from adafruit_pca9685 import PCA9685 
+import time
+i2c_bus = busio.I2C(SCL, SDA) 
+pca = PCA9685(i2c_bus) 
+pca.frequency = 60
+print("Increasing to 50% speed")
+pca.channels[1].duty_cycle = 0x7FFF
+time.sleep(1)
+print("Setting speed to 0")
+pca.channels[1].duty_cycle = 0x0
