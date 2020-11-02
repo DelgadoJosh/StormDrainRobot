757efc62cc1ac8183ecc9fce58ab9464c82b43f3
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-02 17:52:41 -0500

    More notes, actually got it working

--- CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
+++ CS/Josh's Stuff/Prototypes/20-10-25 Motor with PWM/motorsViaController.py
@@ -39,59 +39,122 @@
 # sudo pip install adafruit-circuitpython-pca9685 
 # python3 motorsViaController.py
 
+# import RPi.GPIO as GPIO 
+# GPIO.setmode(GPIO.BOARD)
+
 from board import SCL, SDA
 import busio 
 from adafruit_pca9685 import PCA9685 
 import time
+print("Starting up")
 i2c_bus = busio.I2C(SCL, SDA) 
+print(i2c_bus.scan())
+print("Starting up pca")
 pca = PCA9685(i2c_bus) 
+print("Setting frequency")
 pca.frequency = 60
 
 
-# import RPi.GPIO as GPIO  # Yes, that's RPi, as in Raspberry Pi. Jetson doesn't support PWM.
 
-# print(f"GPIO Mode {GPIO.getmode()}")
-# print(f"GPIO.Board = {GPIO.BOARD}")
-# print(f"GPIO.BCM = {GPIO.BCM}")
-# # Board Numbering Scheme
-# # GPIO.setmode(GPIO.BOARD)
+# # from __future__ import division 
+# import time 
+# # import Adafruit_PCA9685 
+# import adafruit_pca9685
+
+# pca = adafruit_pca9685.PCA9685()
 
 
-# # Disable warning from GPIO
-# GPIO.setwarnings(False)
 
-# leftMotor_DIR_pin = 31
-# GPIO.setup(leftMotor_DIR_pin, GPIO.OUT)
+
+import RPi.GPIO as GPIO  # Yes, that's RPi, as in Raspberry Pi. Jetson doesn't support PWM.
+
+print(f"GPIO Mode {GPIO.getmode()}")
+print(f"GPIO.Board = {GPIO.BOARD}")
+print(f"GPIO.BCM = {GPIO.BCM}")
+# Board Numbering Scheme
+# GPIO.setmode(GPIO.BOARD)
+
+
+# Disable warning from GPIO
+GPIO.setwarnings(False)
+
+# 
+# GPIO.TEGRA_SOC = the mode
+leftMotor_DIR_pin = "GPIO_PZ0"  #6 #200 #31
+GPIO.setup(leftMotor_DIR_pin, GPIO.OUT)
+# GPIO.output(leftMotor_DIR_pin, False)
+GPIO.output(leftMotor_DIR_pin, GPIO.HIGH) 
+
+# # # Completely on
+# # pca.channels[2].duty_cycle = 0xFFFF 
+
+MAX = 0xFFFF
+pwm_pin = 1
+print("Increasing to 50% speed")
+pca.channels[pwm_pin].duty_cycle = int(MAX*0.5)
+time.sleep(3)
+print("Increasing to 100% speed")
+pca.channels[pwm_pin].duty_cycle = MAX
+time.sleep(3)
+print("Reversing direction") 
+GPIO.output(leftMotor_DIR_pin, GPIO.LOW)
+time.sleep(3)
+print("Setting speed to 0")
+pca.channels[pwm_pin].duty_cycle = 0x0
+time.sleep(3)
+print("Reversing direction") 
+GPIO.output(leftMotor_DIR_pin, GPIO.LOW)
+pca.channels[pwm_pin].duty_cycle = MAX
+time.sleep(3)
+print("Setting speed to 0")
+pca.channels[pwm_pin].duty_cycle = 0x0
+
+
+
+# # # Locked antiphase
+# leftMotor_PWM_pin = "GPIO_PZ0"  #6 #200 #31
+# GPIO.setup(leftMotor_PWM_pin, GPIO.OUT)
 # # GPIO.output(leftMotor_DIR_pin, False)
-# GPIO.output(leftMotor_DIR_pin, GPIO.HIGH) 
+# GPIO.output(leftMotor_PWM_pin, GPIO.OUT) 
 
-# Completely on
-# pca.channels[2].duty_cycle = 0xFFFF 
-
+# MAX = 0xFFFF
+# dir_pin = 1
 # print("Increasing to 50% speed")
-# pca.channels[1].duty_cycle = 0x7FFF
-# time.sleep(1)
+# pca.channels[dir_pin].duty_cycle = int(MAX*0.75)
+# time.sleep(3)
+# print("Increasing to 100% speed")
+# pca.channels[dir_pin].duty_cycle = int(MAX)
+# time.sleep(3)
+# print("Reversing direction") 
+# pca.channels[dir_pin].duty_cycle = 0x0
+# time.sleep(3)
 # print("Setting speed to 0")
-# pca.channels[1].duty_cycle = 0x0
+# pca.channels[dir_pin].duty_cycle = 0x0
+
+
+
 
 
 # https://circuitpython.readthedocs.io/projects/pca9685/en/latest/api.html
 
-# Locked anti-phase!
-MAX = 0xFFFF
-pca.channels[2].duty_cycle = 0xFFFF # Set the PWM pin to HIGH to activate locked antiphase
+# # Locked anti-phase!
+# MAX = 0xFFFF
+# dir_pin = 2
+# pwm_pin = 1
+# pca.channels[dir_pin].duty_cycle = 0xFFFF # Set the PWM pin to HIGH to activate locked antiphase
 
-print("increasing to 100% speed in one dir")
-pca.channels[1].duty_cycle = 0x0
-time.sleep(3)
+# print("increasing to 100% speed in one dir")
+# pca.channels[pwm_pin].duty_cycle = 0x0
+# time.sleep(3)
 
-print("decreasing to 0% speed (neutral)")
-pca.channels[1].duty_cycle = 0x7FFF
-time.sleep(1)
+# print("decreasing to 0% speed (neutral)")
+# pca.channels[pwm_pin].duty_cycle = 0x7FFF
+# time.sleep(1)
 
-print("Resetting to 0")
-pca.channels[1].duty_cycle = 0x0
-pca.channels[2].duty_cycle = 0x0
+
+# print("Resetting to 0")
+# pca.channels[pwm_pin].duty_cycle = 0x0
+# pca.channels[dir_pin].duty_cycle = 0x0
 
 
 # Sign-magnitude!
@@ -108,5 +171,14 @@ pca.channels[2].duty_cycle = 0x0
 # time.sleep(2) 
 
 
+
+# Alt version
+# https://github.com/adafruit/Adafruit_Python_PCA9685/blob/master/examples/simpletest.py
+
+# Busio
+# https://circuitpython.readthedocs.io/en/3.x/shared-bindings/busio/__init__.html
+
+# This may be what I'm looking for
+# https://stackoverflow.com/questions/61039191/how-to-setup-gpio-pins-in-gpio-tegra-soc-mode-vs-gpio-bcm-mode-using-jetson-nano
 
 
