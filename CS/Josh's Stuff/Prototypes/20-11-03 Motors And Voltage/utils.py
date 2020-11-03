75ba8839163c29e32b389eac78d1f0da3f8cfb1a
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-03 17:08:37 -0500

    Added another check for utils

--- CS/Josh's Stuff/Prototypes/20-11-03 Motors And Voltage/utils.py
+++ CS/Josh's Stuff/Prototypes/20-11-03 Motors And Voltage/utils.py
@@ -15,6 +15,9 @@ def isFormatted(splitData):
     # In this case it's two floats, separated by one space
     return  len(splitData) >= 2 and isFloat(splitData[0]) and isFloat(splitData[1])
 
+def isInMargins(parsedData):
+    return abs(splitData[0]) <= 1 and abs(splitData[1]) <= 1
+
 def parse(data):
     # This will take the data, split it into a pair of elements needed
     # Returns None if it's not properly parsed
@@ -24,7 +27,10 @@ def parse(data):
         parsedData = []
         parsedData[0] = float(splitData[0]) 
         parsedData[1] = float(splitData[1])
-        return parsedData
+        if (isInMargins(parsedData)):
+            return parsedData 
+        else:
+            return None
     else:
         return None
 
