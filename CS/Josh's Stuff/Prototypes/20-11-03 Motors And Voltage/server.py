6ee6dc4336d82ae111d44f5a3ed074ffcba7f518
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-03 17:09:21 -0500

    Updated server

---
+++ CS/Josh's Stuff/Prototypes/20-11-03 Motors And Voltage/server.py
@@ -0,0 +1,47 @@
+import socket 
+import time 
+import os 
+import random 
+
+# Importing custom libraries
+import motors 
+import utils
+
+class Server():
+    port = 5000
+    def __init__(self , Address=('', port), MaxClient=1):
+        self.s = socket.socket() 
+        self.s.bind(Address)
+        self.s.listen(MaxClient) 
+    
+    def WaitForConnection(self):
+        self.Client, self.Adr = (self.s.accept())
+        print(f"Got a connection from {str(self.Client)}.")
+
+s = Server() 
+print("Server initiated. Waiting for connection.") 
+s.waitForConnection() 
+
+motors.setup()
+
+# Wait for the data, print it, and send it back
+while True:
+    data = s.Client.recv(1024) # Recieve the data from the client
+    print(data)
+
+    if not data:
+        break
+
+    # Move the motors
+    splitData = utils.parse(data)
+    if splitData is not None:
+        motors.setLeftSpeed(splitData[0])
+        motors.setRightSpeed(splitData[1])
+
+    if (data == b"ping"):
+        s.Client.sendall("Pong!".encode('utf-8'))
+        print("Test")
+    else:
+        s.Client.sendall(data) # Ping back the received data
+
+
