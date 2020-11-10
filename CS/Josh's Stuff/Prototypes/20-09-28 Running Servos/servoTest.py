c524c3f82e424ed62c65aba2c153d076908c2127
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-09 20:43:33 -0500

    ServoTest

--- CS/Josh's Stuff/Prototypes/20-09-28 Running Servos/servoTest.py
+++ CS/Josh's Stuff/Prototypes/20-09-28 Running Servos/servoTest.py
@@ -21,15 +21,37 @@
 from adafruit_servokit import ServoKit 
 import time # For pausing
 
-myKit = ServoKit(channels=16)
-myKit.servo[0].angle = 180
+PINS = [0, 1]
+# PIN = 1
+# PIN2 = 0
 
+print("Starting up")
+myKit = ServoKit(channels=16)
+# myKit.servo[PIN].angle = 180
+
+lo = 0
+hi = 180
+for pin in PINS:
+    myKit.servo[pin].actuation_range=hi
+    myKit.servo[pin].set_pulse_width_range(500, 2750) # 1ms to 2ms
+
+
+print("Starting")
 pause_time = .01
 
-for i in range(0, 180, 1):
-    myKit.servo[0].angle = i
+for i in range(lo, hi, 1):
+    print(i)
+    for pin in PINS:
+        myKit.servo[pin].angle = i
+    # myKit.servo[PIN].angle = i
+    # myKit.servo[PIN2].angle = i
     time.sleep(pause_time)
 
-for i in range(180, 0, -1):
-    myKit.servo[0].angle = i
+time.sleep(1)
+for i in range(hi, lo, -1):
+    print(i)
+    for pin in PINS:
+        myKit.servo[pin].angle = i
+    # myKit.servo[PIN].angle = i
+    # myKit.servo[PIN2].angle = i
     time.sleep(pause_time)
