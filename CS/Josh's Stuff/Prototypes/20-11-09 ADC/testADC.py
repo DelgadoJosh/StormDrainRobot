17a207e4cac0930ff56269143f25f49a30e6c3fb
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-11 20:02:57 -0500

    Added print statement for ADC and changed pin

--- CS/Josh's Stuff/Prototypes/20-11-09 ADC/testADC.py
+++ CS/Josh's Stuff/Prototypes/20-11-09 ADC/testADC.py
@@ -6,6 +6,7 @@ import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
+print("Beginning ADC")
 ads = ADS.ADS1115(i2c)
 ads.mode = ADS.Mode.CONTINUOUS
 ads.gain = 1
@@ -15,7 +16,7 @@ R2 = 7500
 
 
 while(True):
-    chan = AnalogIn(ads, ADS.P1)
+    chan = AnalogIn(ads, ADS.P0)
 
     Vout = chan.voltage 
     Vin = Vout * (R1 + R2)/R2
