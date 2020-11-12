babc2eaee8bcf714f55c2abeeeb7d46df1571e4c
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-11 19:51:23 -0500

    Updated server and client to work

--- CS/Josh's Stuff/Prototypes/20-11-11 Tandem/client.py
+++ CS/Josh's Stuff/Prototypes/20-11-11 Tandem/client.py
@@ -13,7 +13,7 @@ payload_size = struct.calcsize("Q")
 class Client_Viewer():
   # ip_address = "10.0.0.2"
   ip_address = "localhost"
-  port = 4000
+  port = 5000
   def __init__(self, Address=(ip_address, port)):
     self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.s.connect(Address)
@@ -58,21 +58,26 @@ def showVideo():
 
             # print(frame_data)
 
-            # Extract frame
-            # frame = pickle.loads(frame_data)
+            # # Extract frame
+            # # frame = pickle.loads(frame_data)
+            # frameBytes = base64.b64decode(frame_data) # If doing the raw encoded data
+            # # print(frameBytes)
+            # # print(type(frameBytes))
+            # # frame = Image.open(frameBytes)
+            # img_as_np = np.frombuffer(frameBytes, dtype=np.uint8)
+            # # frame = cv2.imdecode(img_as_np, cv2.IMREAD_COLOR)
+            # frame = cv2.imdecode(img_as_np, flags=1)
+            # # cv2.imwrite("./0.jpg", frame)
+
+            # If going the direct encode/decode to get frameBytes
             frameBytes = base64.b64decode(frame_data) # If doing the raw encoded data
-            # print(frameBytes)
-            # print(type(frameBytes))
-            # frame = Image.open(frameBytes)
-            img_as_np = np.frombuffer(frameBytes, dtype=np.uint8)
-            # frame = cv2.imdecode(img_as_np, cv2.IMREAD_COLOR)
-            frame = cv2.imdecode(img_as_np, flags=1)
-            # cv2.imwrite("./0.jpg", frame)
 
             # If going the json route
-            # jsonData = json.load(data.decode('utc-8'))
-            # frame = base64.b64decode(jsonData['img'])
-            
+            jsonData = json.load(data.decode('utc-8'))
+            frameBytes = base64.b64decode(jsonData['img'])
+
+            img_as_np = np.frombuffer(frameBytes, dtype=np.uint8)
+            frame = cv2.imdecode(img_as_np, flags=1)
 
             # Display
             cv2.imshow("Frame", frame)
