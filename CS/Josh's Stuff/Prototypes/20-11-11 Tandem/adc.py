c603bd32a83c4c6a216e0103f5e7cb090247c9a3
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-11 20:24:26 -0500

    Added adc util to grab data

---
+++ CS/Josh's Stuff/Prototypes/20-11-11 Tandem/adc.py
@@ -0,0 +1,29 @@
+# Analog to Digital Converter
+# Used to measure the voltage in the board
+
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS 
+from adafruit_ads1x15.analog_in import AnalogIn
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+print("Beginning ADC")
+ads = ADS.ADS1115(i2c)
+ads.mode = ADS.Mode.CONTINUOUS
+ads.gain = 1
+
+R1 = 30000
+R2 = 7500
+
+# Vin = Vout * (R1 + R2)/R2
+coefficient = 1.0*(R1 + R2)/R2
+
+def getVoltage():
+    chan = AnalogIn(ads, ADS.P0)
+
+    Vout = chan.voltage
+    Vin = Vout * coefficient 
+    return Vin
+
