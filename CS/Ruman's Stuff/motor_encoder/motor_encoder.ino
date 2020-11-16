7494dc02b048b6f30d37df7a978a3ca21de012f4
RumanRashid <42753967+RumanRashid@users.noreply.github.com>
2020-11-15 19:23:16 -0500

    motor encoder
    the code for the motor encoder (using teensyduino)

---
+++ CS/Ruman's Stuff/motor_encoder/motor_encoder.ino
@@ -0,0 +1,30 @@
+/* Encoder Library - Basic Example
+ * http://www.pjrc.com/teensy/td_libs_Encoder.html
+ *
+ * This example code is in the public domain.
+ */
+
+#include <Encoder.h>
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(5, 6);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+}
+
+float oldPosition  = -999;
+
+void loop() {
+  float newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    newPosition=newPosition/960;
+    Serial.println(newPosition);
+  }
+}
