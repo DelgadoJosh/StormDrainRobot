4810e8d4c3b14e44acc3ac561cd331750637ed60
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-20 18:45:16 -0500

    Updated Servos

--- CS/Josh's Stuff/Prototypes/20-11-11 Tandem/servos.py
+++ CS/Josh's Stuff/Prototypes/20-11-11 Tandem/servos.py
@@ -1,29 +1,15 @@
 # Custom Library to adjust the servos
 # https://www.youtube.com/watch?v=8YKAtpPSEOk
-from adafruit_servokit import ServoKit 
-# import time # For pausing
-# from board import SCL, SDA
-# import busio 
-# from adafruit_pca9685 import PCA9685 
-# import time
+from adafruit_servokit import ServoKit
+import time 
 
-# print("Starting up i2c_bus")
-# i2c_bus = busio.I2C(SCL, SDA) 
-# print(i2c_bus.scan())
-
-# print("Starting up PCA")
-# pca = PCA9685(i2c_bus) 
-
-# print("Setting Frequency for PCA")
-# pca.frequency = 369
-
-HORIZONTAL_PIN = 4
-VERTICAL_PIN = 5
-# PWM_PIN = 0
+HORIZONTAL_PIN = 0 # 4 
+VERTICAL_PIN = 1 # 5
 PINS = [HORIZONTAL_PIN, VERTICAL_PIN]
 
 print("Starting up servo kit")
-myKit = ServoKit(channels=16, frequency=369) # 369   370 too high
+# myKit = ServoKit(channels=16, frequency=60) # 369   370 too high
+myKit = ServoKit(channels=16, frequency=60, address=0x41)
 
 # lo = 0
 # hi = 180
