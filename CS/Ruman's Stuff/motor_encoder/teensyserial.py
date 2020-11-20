bd51cf87064e666ef704afcbbb611589ccd1cfec
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-20 18:36:20 -0500

    updated TeensySerial.py

--- CS/Ruman's Stuff/motor_encoder/teensyserial.py
+++ CS/Ruman's Stuff/motor_encoder/teensyserial.py
@@ -1,13 +1,22 @@
+# /dev/ttyTHS1 = jetson port
+
+# sudo python3 teensyserial.py
+# ls -l /dev/ttyTHS1
+# groups
+# sudo usermod -aG dialout teamblack
+
+# This makes it so everyone has read/write permissions
+# sudo chmod 666 /dev/ttyTHS1
 import time
 import serial
 import random       
 ser = serial.Serial(            
-port='/dev/ttyS0',
-baudrate = 9600,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
+    port='/dev/ttyTHS1', #'/dev/ttyS0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 while True:
     time.sleep(1)
