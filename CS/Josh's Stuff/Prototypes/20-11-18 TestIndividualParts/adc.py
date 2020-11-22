d2e3181a12410e94514455a1762464175684be6f
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-11-22 12:55:53 -0500

    Tuning ADC

--- CS/Josh's Stuff/Prototypes/20-11-18 TestIndividualParts/adc.py
+++ CS/Josh's Stuff/Prototypes/20-11-18 TestIndividualParts/adc.py
@@ -14,9 +14,12 @@ ads = ADS.ADS1115(i2c)
 ads.mode = ADS.Mode.CONTINUOUS
 ads.gain = 1
 
-R1 = 30000
-R2 = 7500
+R1 = 29573 # 30000
+R2 = 7300 # 7500
 
+# Vin*R2/Vout - R2 = R1 
+# print((13.23 * R2)/Vout -R2)
+ 
 # Vin = Vout * (R1 + R2)/R2
 coefficient = 1.0*(R1 + R2)/R2
 
