41033c49d98c8f719b6031e50cae64c61586e22f
Senior Design 2020 <ucfseniordesign2020@gmail.com>
2020-12-01 15:46:23 -0500

    Added attachment support for testing

---
+++ CS/Josh's Stuff/Prototypes/20-11-18 TestIndividualParts/attachment.py
@@ -0,0 +1,43 @@
+# https://www.youtube.com/watch?v=8YKAtpPSEOk
+import time # For pausing
+from board import SCL, SDA
+import busio 
+from adafruit_pca9685 import PCA9685 
+import time
+
+LIGHTS_PIN = 7
+MAX = 0xFFFF
+
+print("Starting up")
+i2c_bus = busio.I2C(SCL, SDA) 
+print(i2c_bus.scan())
+
+print("Starting up pca")
+pca = PCA9685(i2c_bus) 
+
+print("Setting frequency for attachment")
+pca.frequency = 1526
+
+def setPWM(percentPower):
+    power = abs(int(MAX*percentPower))
+    print(f"Setting power to {percentPower}")
+    pca.channels[LIGHTS_PIN].duty_cycle = power
+
+def testAttachment():
+    print("Beginning Attachment Test")
+    
+    print("Setting to 0% power initially for 1 sec")
+    setPWM(0)
+    time.sleep(1)
+
+    for i in range(100):
+        setPWM(i / 100.0)
+        time.sleep(0.1)
+
+    print()
+    print("Ending Test")
+    print()
+    setPWM(0)
+
+if __name__ == '__main__':
+    testAttachment()
